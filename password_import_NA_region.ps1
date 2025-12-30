43a47c6f36141e24f508335ae9945e5698f2f068
IT Glue Tech Support <itgluetech@gmail.com>
2025-12-30 11:04:12 -0800

    Refactor authorization token and related functions

--- password_import_NA_region.ps1
+++ password_import_NA_region.ps1
@@ -252,14 +252,14 @@ $pass_create_body = @{
     data = @{
         type = "passwords"
         attributes = @{
-            "organization-id"      = $password_org_id.Trim()
+            "organization-id"      = $password_org_id
             name                   = $password_name.Trim()
             username               = $password_username.Trim()
             password               = $password_value.Trim()
             url                    = $password_url.Trim()
             notes                  = $password_notes.Trim()
-            "password-category-id" = $password_category_id.Trim()
-            "password-folder-id"   = $password_folder_id.Trim()
+            "password-category-id" = $password_category_id
+            "password-folder-id"   = $password_folder_id
             otpSecret              = $password_otp.Trim()
         }
     }
@@ -367,6 +367,7 @@ if ($access_token -eq $null){
     
 
 }
+
 
 
 
