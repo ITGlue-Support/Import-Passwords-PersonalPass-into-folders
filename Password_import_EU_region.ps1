2c658e6a575d0d4dfbb55012de02a11caeb03fc9
IT Glue Tech Support <itgluetech@gmail.com>
2026-02-25 11:43:08 -0800

    Refactor authorization and password management functions

--- Password_import_EU_region.ps1
+++ Password_import_EU_region.ps1
@@ -253,7 +253,8 @@ function create_password {
         [string]$password_url,
         [string]$password_notes,
         [string]$password_folder_id,
-        [string]$password_category_id
+        [string]$password_category_id,
+        [string]$personal
     )
 
 
@@ -270,6 +271,7 @@ $pass_create_body = @{
             "password-category-id" = $password_category_id
             "password-folder-id"   = $password_folder_id
             otpSecret              = $password_otp.Trim()
+            personal               = $personal
         }
     }
 } | ConvertTo-Json -Depth 5
@@ -332,7 +334,7 @@ function extract_csv {
 
         Write-Host $pass_org_id $($passwords.name) $($passwords.username) $($passwords.password) $($passwords.otp_secret) $($passwords.url) $pass_folder_id $pass_type_id
 
-        create_password -password_org_id "$pass_org_id" -password_name "$($passwords.name)" -password_username "$($passwords.username)" -password_value "$($passwords.password)" -password_otp "$($passwords.otp_secret)" -password_url "$($passwords.url)" -password_notes "$($passwords.notes)" -password_folder_id "$pass_folder_id" -password_category_id "$pass_type_id"
+        create_password -password_org_id "$pass_org_id" -password_name "$($passwords.name)" -password_username "$($passwords.username)" -password_value "$($passwords.password)" -password_otp "$($passwords.otp_secret)" -password_url "$($passwords.url)" -password_notes "$($passwords.notes)" -password_folder_id "$pass_folder_id" -password_category_id "$pass_type_id" -personal "$($passwords.personal)
     
     
     }
@@ -374,6 +376,7 @@ if ($access_token -eq $null){
     $access_token = request_data
     
 }
+
 
 
 
